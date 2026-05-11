ed5ca312a774b51b5d6645ea8e586c8ec4da4ef4
Alban Siffer <31479857+asiffer@users.noreply.github.com>
2026-05-11 14:44:11 +0200

    add main.html in integrity

--- internal/integrity.py
+++ internal/integrity.py
@@ -10,6 +10,15 @@ from bs4 import BeautifulSoup
 from internal.common import THEME_PATH, info, log
 
 
+def get_templates():
+    out = {THEME_PATH / "main.html"}
+    for file in os.listdir(THEME_PATH / "templates"):
+        fp = THEME_PATH / "templates" / file
+        if file.endswith(".html"):
+            out.add(fp)
+    return out
+
+
 def integrity(
     force: Annotated[
         bool, typer.Option(help="Force update of all integrity attributes")
@@ -22,28 +31,26 @@ def integrity(
     if not force:
         attrs_filter["integrity"] = False
 
-    for file in os.listdir(THEME_PATH / "templates"):
-        fp = THEME_PATH / "templates" / file
-        if file.endswith(".html"):
-            save = False
-            with open(fp, "r") as f:
-                content = f.read()
-                soup = BeautifulSoup(content, "html.parser")
-                for script in soup.find_all("script", attrs=attrs_filter):  # type: ignore
-                    src = script["src"]
-                    if not isinstance(src, str):
-                        continue
+    for file in get_templates():
+        save = False
+        with open(file, "r") as f:
+            content = f.read()
+            soup = BeautifulSoup(content, "html.parser")
+            for script in soup.find_all("script", attrs=attrs_filter):  # type: ignore
+                src = script["src"]
+                if not isinstance(src, str):
+                    continue
 
-                    match = re.search(r"[']([a-zA-Z0-9_./-]+[.]js)[']", src)
-                    if match:
-                        source = match.group(1)
-                        log(f"Processing script: {source} in file {file}")
-                        with open(THEME_PATH / source, "rb") as f:
-                            a = b64encode(sha384(f.read()).digest())
-                            integrity = f"sha384-{a.decode()}"
-                            script["integrity"] = integrity
-                            save = True
+                match = re.search(r"[']([a-zA-Z0-9_./-]+[.]js)[']", src)
+                if match:
+                    source = match.group(1)
+                    log(f"Processing script: {source} in file {file}")
+                    with open(THEME_PATH / source, "rb") as f:
+                        a = b64encode(sha384(f.read()).digest())
+                        integrity = f"sha384-{a.decode()}"
+                        script["integrity"] = integrity
+                        save = True
             if save:
-                with open(fp, "w") as f:
+                with open(file, "w") as f:
                     f.write(str(soup))
                 info(f"Updated integrity attributes in {file}")
